c5c44a906003cc68bdb4810aa20f9878ff440bcd
jeremy.babouche <jeremy.babouche@f222467b-8754-c790-d948-9a5a32e0f2da>
2012-06-25 21:09:43 +0000

    Avancement du MapEditor : Presque terminé Ajouts futurs : - légère amélioration du design - ajout des fonctionnalités d'enregistrement et de chargement quand le serveur sera avancé

--- trunk/MapEditor/MapEditor/SelectTexture.xaml.cs
+++ trunk/MapEditor/MapEditor/SelectTexture.xaml.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System;
 
 namespace MapEditor
 {
@@ -18,9 +11,86 @@ namespace MapEditor
     /// </summary>
     public partial class SelectTexture : Window
     {
-        public SelectTexture()
+        public ObservableCollection<Texture> Textures
+        {
+            get;
+            private set;
+        }
+
+        public delegate void TextureSelectedEventHandler(int type, BitmapImage image);
+        public event TextureSelectedEventHandler TextureSelected;
+
+        private int _type = -1;
+
+        private void NotifyTextureSelected(BitmapImage image)
+        {
+            if (TextureSelected != null)
+                TextureSelected(_type, image);
+        }
+
+        public SelectTexture(byte type)
         {
             InitializeComponent();
+
+            listTextures.MouseDoubleClick += new System.Windows.Input.MouseButtonEventHandler(listTextures_MouseDoubleClick);
+
+            _type = type;
+            string currentDir = Directory.GetCurrentDirectory();
+            string textureDir = Path.Combine(currentDir, "Textures");
+            string dirName = string.Empty;
+            switch (type)
+            {
+                case 0:
+                    dirName = "Sols";
+                    this.Title = "Textures du sol";
+                    break;
+                case 1:
+                    dirName = "Dectructibles";
+                    this.Title = "Textures des blocs destructibles";
+                    break;
+                case 2:
+                    dirName = "Indectructibles";
+                    this.Title = "Textures des blocs indestructibles";
+                    break;
+                default:
+                    break;
+            }
+            string currentTextureDir = Path.Combine(textureDir, dirName);
+            Textures = new ObservableCollection<Texture>();
+            string[] dirs = Directory.GetDirectories(currentTextureDir);
+            for (int i = 0; i < dirs.Length; ++i)
+            {
+                string[] files = Directory.GetFiles(dirs[i]);
+                for (int j = 0; j < files.Length; ++j)
+                {
+                    Textures.Add(new Texture(files[j]));
+                }
+            }
+        }
+
+        void listTextures_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (listTextures.SelectedItem != null && listTextures.SelectedItem is Texture)
+            {
+                NotifyTextureSelected(((Texture)listTextures.SelectedItem).Image);
+            }
+
+            this.Close();
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            if (listTextures.SelectedItem != null && listTextures.SelectedItem is Texture)
+            {
+                NotifyTextureSelected(((Texture)listTextures.SelectedItem).Image);
+            }
+
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
