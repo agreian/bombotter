3a52db1443e358769e8ff410decf59a0ac914d92
jeremy.babouche <jeremy.babouche@f222467b-8754-c790-d948-9a5a32e0f2da>
2012-06-25 21:09:43 +0000

    Avancement du MapEditor : Presque terminé Ajouts futurs : - légère amélioration du design - ajout des fonctionnalités d'enregistrement et de chargement quand le serveur sera avancé

--- MapEditor/MapEditor/MainWindow.xaml.cs
+++ MapEditor/MapEditor/MainWindow.xaml.cs
@@ -20,8 +20,9 @@ namespace MapEditor
     public partial class MainWindow : Window
     {
         private byte[,] _map = new byte[13, 11];
-        private Label[,] _imgs = new Label[13, 11];
+        private Image[,] _imgs = new Image[13, 11];
         private int _currentX = 0, _currentY = 0;
+        private BitmapImage[] _bmps = new BitmapImage[3];
 
         public MainWindow()
         {
@@ -40,28 +41,23 @@ namespace MapEditor
                 for (int j = 0; j < 11; ++j)
                 {
                     _map[i, j] = 0;
-                    _imgs[i, j] = new Label();
+                    //Button btn = new Button();
+                    //btn.Background = Brushes.White;
+                    //btn.Style = Style.
+                    _imgs[i, j] = new Image();
+                    //btn.Content = _imgs[i, j];
                     gridView.Children.Add(_imgs[i, j]);
-                    _imgs[i, j].Content = _map[i, j].ToString();
-                    switch (_map[i,j])
-                    {
-                        case 0:
-                            optBlank.IsChecked = true;
-                            break;
-                        case 1:
-                            optBreakable.IsChecked = true;
-                            break;
-                        case 2:
-                            optUnbreakable.IsChecked = true;
-                            break;
-                        default:
-                            break;
-                    }
+                    //gridView.Children.Add(btn);
 
                     _imgs[i, j].MouseLeftButtonDown += new MouseButtonEventHandler(img_MouseLeftButtonDown);
+                    //btn.MouseLeftButtonDown += new MouseButtonEventHandler(img_MouseLeftButtonDown);
+                    _imgs[i, j].Stretch = Stretch.Fill;
+                    _imgs[i, j].StretchDirection = StretchDirection.Both;
 
                     Grid.SetColumn(_imgs[i, j], i);
                     Grid.SetRow(_imgs[i, j], j);
+                    //Grid.SetColumn(btn, i);
+                    //Grid.SetRow(btn, j);
                 }
             }
         }
@@ -79,23 +75,71 @@ namespace MapEditor
 
         private void btnValidate_Click(object sender, RoutedEventArgs e)
         {
-            if(optBlank.IsChecked.Value)
+            if (optBlank.IsChecked.Value)
             {
                 _map[_currentX, _currentY] = 0;
             }
-            else if(optBreakable.IsChecked.Value)
+            else if (optBreakable.IsChecked.Value)
             {
                 _map[_currentX, _currentY] = 1;
             }
-            else if(optUnbreakable.IsChecked.Value)
+            else if (optUnbreakable.IsChecked.Value)
             {
                 _map[_currentX, _currentY] = 2;
             }
-
-            _imgs[_currentX, _currentY].Content = _map[_currentX, _currentY];
+            
+            _imgs[_currentX, _currentY].Source = _bmps[_map[_currentX, _currentY]];
         }
 
         private void btnSelectTexture_Click(object sender, RoutedEventArgs e)
+        {
+            this.IsEnabled = false;
+            SelectTexture window;
+            if (optBlank.IsChecked.Value)
+            {
+                window = new SelectTexture(0);
+            }
+            else if (optBreakable.IsChecked.Value)
+            {
+                window = new SelectTexture(1);
+            }
+            else
+            {
+                window = new SelectTexture(2);
+            }
+
+            window.Show();
+            window.Closed += new EventHandler(window_Closed);
+            window.TextureSelected += new SelectTexture.TextureSelectedEventHandler(window_TextureSelected);
+        }
+
+        void window_TextureSelected(int type, BitmapImage image)
+        {
+            _bmps[type] = image;
+
+            for (int i = 0; i < 13; ++i)
+            {
+                for (int j = 0; j < 11; ++j)
+                {
+                    if (_map[i, j] == type)
+                    {
+                        _imgs[i, j].Source = image;
+                    }
+                }
+            }
+        }
+
+        void window_Closed(object sender, EventArgs e)
+        {
+            this.IsEnabled = true;
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
         {
 
         }
