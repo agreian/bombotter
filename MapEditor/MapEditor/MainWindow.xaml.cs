c1de830ecb81a1adc36b4417b0d3048f60a2d490
jeremy.babouche <jeremy.babouche@f222467b-8754-c790-d948-9a5a32e0f2da>
2012-06-26 16:40:02 +0000

    Version finale de l'éditeur de MapEditor Amélioration possible : - Envoi direct sur le serveur des maps - Choix des SpawnPoints

--- MapEditor/MapEditor/MainWindow.xaml.cs
+++ MapEditor/MapEditor/MainWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
 
 namespace MapEditor
 {
@@ -21,13 +23,18 @@ namespace MapEditor
     {
         private byte[,] _map = new byte[13, 11];
         private Image[,] _imgs = new Image[13, 11];
-        private int _currentX = 0, _currentY = 0;
-        private BitmapImage[] _bmps = new BitmapImage[3];
+
+        private List<Selection> _selectedElement = new List<Selection>();
+        private Texture[] _bmps = new Texture[3];
 
         public MainWindow()
         {
             InitializeComponent();
 
+            for (int i = 0; i < 3; ++i)
+            {
+                _bmps[i] = new Texture(System.IO.Path.Combine(Directory.GetCurrentDirectory(), @"Resources\blank.jpg"));
+            }
             for (int i = 0; i < 13; ++i)
             {
                 gridView.ColumnDefinitions.Add(new ColumnDefinition());
@@ -45,31 +52,108 @@ namespace MapEditor
                     //btn.Background = Brushes.White;
                     //btn.Style = Style.
                     _imgs[i, j] = new Image();
+                    _imgs[i, j].Source = _bmps[0].Image;
                     //btn.Content = _imgs[i, j];
-                    gridView.Children.Add(_imgs[i, j]);
+                    Border border = new Border();
+                    border.BorderBrush = Brushes.Black;
+                    border.BorderThickness = new Thickness(2);
+                    border.Child = _imgs[i, j];
+                    //gridView.Children.Add(_imgs[i, j]);
+                    gridView.Children.Add(border);
                     //gridView.Children.Add(btn);
 
-                    _imgs[i, j].MouseLeftButtonDown += new MouseButtonEventHandler(img_MouseLeftButtonDown);
+                    //_imgs[i, j].MouseLeftButtonDown += new MouseButtonEventHandler(img_MouseLeftButtonDown);
+                    border.MouseLeftButtonDown += new MouseButtonEventHandler(img_MouseLeftButtonDown);
+                    border.MouseEnter += new MouseEventHandler(border_MouseEnter);
+                    border.MouseLeave += new MouseEventHandler(border_MouseLeave);
+                    //border.MouseLeftButtonDown += new MouseButtonEventHandler(img_MouseLeftButtonDown);
+                    //border.Mouse
+                    //border.PreviewMouseLeftButtonDown += new MouseButtonEventHandler(img_MouseLeftButtonDown);
                     //btn.MouseLeftButtonDown += new MouseButtonEventHandler(img_MouseLeftButtonDown);
                     _imgs[i, j].Stretch = Stretch.Fill;
                     _imgs[i, j].StretchDirection = StretchDirection.Both;
 
-                    Grid.SetColumn(_imgs[i, j], i);
-                    Grid.SetRow(_imgs[i, j], j);
+                    //Grid.SetColumn(_imgs[i, j], i);
+                    //Grid.SetRow(_imgs[i, j], j);
+                    Grid.SetColumn(border, i);
+                    Grid.SetRow(border, j);
                     //Grid.SetColumn(btn, i);
                     //Grid.SetRow(btn, j);
                 }
             }
         }
 
+        void border_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (sender is Border)
+            {
+                Border temp = (Border)sender;
+
+                if (temp.BorderBrush == Brushes.DarkBlue)
+                {
+                    temp.BorderBrush = Brushes.Black;
+                }
+            }
+        }
+
+        void border_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (sender is Border)
+            {
+                Border temp = (Border)sender;
+
+                if (temp.BorderBrush == Brushes.Black)
+                {
+                    temp.BorderBrush = Brushes.DarkBlue;
+                }
+            }
+        }
+
         void img_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (sender is UIElement)
+            if (sender is Border)
             {
-                _currentX = Grid.GetColumn((UIElement)sender);
-                _currentY = Grid.GetRow((UIElement)sender);
+                Border temp = (Border)sender;
 
-                //MessageBox.Show(string.Format("x : {0}, y : {1}", x, y));
+                if (_selectedElement.Count == 0)
+                {
+                    int x = Grid.GetColumn((UIElement)sender), y = Grid.GetRow((UIElement)sender);
+
+                    switch (_map[x, y])
+                    {
+                        case 0:
+                            optBlank.IsChecked = true;
+                            break;
+                        case 1:
+                            optBreakable.IsChecked = true;
+                            break;
+                        case 2:
+                            optUnbreakable.IsChecked = true;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                if (temp.BorderBrush == Brushes.Red)
+                {
+                    temp.BorderBrush = Brushes.Black;
+
+                    for (int i = 0; i < _selectedElement.Count; ++i)
+                    {
+                        if (_selectedElement[i].Image == temp)
+                        {
+                            _selectedElement.RemoveAt(i);
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    temp.BorderBrush = Brushes.Red;
+
+                    _selectedElement.Add(new Selection(temp, Grid.GetColumn((UIElement)sender), Grid.GetRow((UIElement)sender)));
+                }
             }
         }
 
@@ -77,18 +161,35 @@ namespace MapEditor
         {
             if (optBlank.IsChecked.Value)
             {
-                _map[_currentX, _currentY] = 0;
+                for (int i = 0; i < _selectedElement.Count; ++i)
+                {
+                    _map[_selectedElement[i].X, _selectedElement[i].Y] = 0;
+                }
             }
             else if (optBreakable.IsChecked.Value)
             {
-                _map[_currentX, _currentY] = 1;
-            }
+                for (int i = 0; i < _selectedElement.Count; ++i)
+                {
+                    if (TestPosition(_selectedElement[i].X, _selectedElement[i].Y))
+                        _map[_selectedElement[i].X, _selectedElement[i].Y] = 1;
+                }
+            } 
             else if (optUnbreakable.IsChecked.Value)
             {
-                _map[_currentX, _currentY] = 2;
+                for (int i = 0; i < _selectedElement.Count; ++i)
+                {
+                    if (TestPosition(_selectedElement[i].X, _selectedElement[i].Y))
+                        _map[_selectedElement[i].X, _selectedElement[i].Y] = 2;
+                }
             }
-            
-            _imgs[_currentX, _currentY].Source = _bmps[_map[_currentX, _currentY]];
+
+            for (int i = 0; i < _selectedElement.Count; ++i)
+            {
+                _imgs[_selectedElement[i].X, _selectedElement[i].Y].Source = _bmps[_map[_selectedElement[i].X, _selectedElement[i].Y]].Image;
+                _selectedElement[i].Image.BorderBrush = Brushes.Black;
+            }
+
+            _selectedElement.Clear();
         }
 
         private void btnSelectTexture_Click(object sender, RoutedEventArgs e)
@@ -113,7 +214,7 @@ namespace MapEditor
             window.TextureSelected += new SelectTexture.TextureSelectedEventHandler(window_TextureSelected);
         }
 
-        void window_TextureSelected(int type, BitmapImage image)
+        void window_TextureSelected(int type, Texture image)
         {
             _bmps[type] = image;
 
@@ -123,10 +224,17 @@ namespace MapEditor
                 {
                     if (_map[i, j] == type)
                     {
-                        _imgs[i, j].Source = image;
+                        _imgs[i, j].Source = image.Image;
                     }
                 }
             }
+
+            for (int i = 0; i < _selectedElement.Count; ++i)
+            {
+                _selectedElement[i].Image.BorderBrush = Brushes.Black;
+            }
+
+            _selectedElement.Clear();
         }
 
         void window_Closed(object sender, EventArgs e)
@@ -134,14 +242,164 @@ namespace MapEditor
             this.IsEnabled = true;
         }
 
+        private bool TestPosition(int x, int y)
+        {
+            if (
+                (x == 0 && y == 0) ||
+                (x == 1 && y == 0) ||
+                (x == 0 && y == 1) ||
+                (x == 12 && y == 0) ||
+                (x == 11 && y == 0) ||
+                (x == 12 && y == 1) ||
+                (x == 0 && y == 10) ||
+                (x == 1 && y == 10) ||
+                (x == 0 && y == 9) ||
+                (x == 12 && y == 10) ||
+                (x == 11 && y == 10) ||
+                (x == 12 && y == 9)
+                )
+                return false;
+            else
+                return true;
+        }
+
         private void btnLoad_Click(object sender, RoutedEventArgs e)
         {
+            // Configure open file dialog box
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = ".map";
+            dlg.Filter = "Fichier map (.map)|*.map";
 
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+
+                using (StreamReader sr = new StreamReader(filename))
+                {
+                    for (int j = 0; j < 11; ++j)
+                    {
+                        string line = sr.ReadLine();
+                        string[] chars = line.Split(';');
+                        for (int i = 0; i < 13; ++i)
+                        {
+                            _map[i, j] = byte.Parse(chars[i]);
+                        }
+                    }
+
+                    for (int i = 0; i < _bmps.Length; ++i)
+                    {
+                        string line = sr.ReadLine();
+
+                        string file = line.Split('_')[1];
+                        string textDir = string.Empty;
+
+                        switch (i)
+                        {
+                            case 0:
+                                textDir = "Sols";
+                                break;
+                            case 1:
+                                textDir = "Destructibles";
+                                break;
+                            case 2:
+                                textDir = "Indestructibles";
+                                break;
+                            default:
+                                break;
+                        }
+
+                        _bmps[i] = new Texture(System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "Textures", textDir, file));
+                    }
+
+                    string nameLine = sr.ReadLine();
+
+                    txtName.Text = nameLine.Replace("#Name_", string.Empty);
+                }
+
+                for (int i = 0; i < 13; ++i)
+                {
+                    for (int j = 0; j < 11; ++j)
+                    {
+                        _imgs[i, j].Source = _bmps[_map[i, j]].Image;
+                    }
+                }
+
+                for (int i = 0; i < _selectedElement.Count; ++i)
+                {
+                    _selectedElement[i].Image.BorderBrush = Brushes.Black;
+                }
+
+                _selectedElement.Clear();
+            }
         }
 
         private void btnSave_Click(object sender, RoutedEventArgs e)
         {
+            for (int i = 0; i < _bmps.Length; ++i)
+            {
+                if (System.IO.Path.GetFileNameWithoutExtension(_bmps[i].Path) == "blank" && (new System.IO.DirectoryInfo(_bmps[i].Path)).Parent.ToString() == "Resources")
+                {
+                    MessageBox.Show("Veuillez sélectionner des textures avant d'enregistrer", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+            }
 
+            if(txtName.Text == "Nom de la carte" || txtName.Text == string.Empty)
+            {
+                MessageBox.Show("Veuillez indiquer un nom pour la map", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // Configure save file dialog box
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.DefaultExt = ".map";
+            dlg.Filter = "Fichier map (.map)|*.map";
+
+            // Show save file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save document
+                string filename = dlg.FileName;
+
+                using (StreamWriter sw = new StreamWriter(filename))
+                {
+                    for (int j = 0; j < 11; ++j)
+                    {
+                        for (int i = 0; i < 13; ++i)
+                        {
+                            sw.Write(string.Format("{0};", _map[i, j]));
+                        }
+                        sw.WriteLine();
+                    }
+
+                    for (int i = 0; i < _bmps.Length; ++i)
+                    {
+                        sw.WriteLine(string.Format(@"#{0}_{1}\{2}", i, (new DirectoryInfo(_bmps[i].Path)).Parent, System.IO.Path.GetFileName(_bmps[i].Path)));
+                    }
+
+                    sw.WriteLine(string.Format("#Name_{0}", txtName.Text));
+                }
+            }
+        }
+
+        private void TextBox_IsMouseCapturedChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (sender is TextBox)
+            {
+                TextBox temp = (TextBox)sender;
+                if (temp.Text == "Nom de la carte")
+                {
+                    temp.Text = string.Empty;
+                }
+            }
         }
     }
 }
---
+++ MapEditor/MapEditor/Texture.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Windows.Media.Imaging;
+using System.IO;
+
+namespace MapEditor
+{
+    public class Texture : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        public BitmapImage Image
+        {
+            get
+            {
+                return _image;
+            }
+            private set
+            {
+                _image = value;
+                NotifyPropertyChanged("Image");
+            }
+        }
+        private BitmapImage _image;
+
+        public string Label
+        {
+            get
+            {
+                return _label;
+            }
+            private set
+            {
+                _label = value;
+                NotifyPropertyChanged("Label");
+            }
+        }
+        private string _label;
+
+        public string Path
+        {
+            get;
+            private set;
+        }
+
+        public Texture(string path)
+        {
+            this.Path = path;
+            this.Label = string.Format("{0} {1}", (new DirectoryInfo(path)).Parent.ToString(), System.IO.Path.GetFileNameWithoutExtension(path));
+            this.Image = new BitmapImage(new Uri(path, UriKind.RelativeOrAbsolute));
+        }
+    }
+}
