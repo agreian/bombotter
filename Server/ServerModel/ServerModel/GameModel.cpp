fa60b04e281c8fa25f248f7f89fbda5863275ff6
florent.brouca <florent.brouca@gmail.com>
2012-06-28 15:52:56 +0000

    Commit du game Model

--- Server/ServerModel/ServerModel/GameModel.cpp
+++ Server/ServerModel/ServerModel/GameModel.cpp
@@ -1,17 +1,41 @@
 #include "GameModel.h"
+#include "UserModel.h"
 
-GameModel::GameModel()
+/*GameModel::GameModel()
 {
-}
+	
+}*/
 
 GameModel::~GameModel()
-{}
+{
+	
+}
+
+GameModel::GameModel(UserModel* creator, ::Ice::ObjectAdapterPtr obj)
+{
+	this->gameCreator = creator;
+}
 
 void GameModel::addBot()
-{}
+{
+	/* Créer un UserModel avec isBot à true ! (classe à modifier)
+	Puis ajouter à la liste des players le bot en question
+	 */
+	UserModel* user = UserModel::CreateUser("bot1", "pwdBot1", true);
+	this->addUser(user);
+}
+
+void GameModel::addUser(UserModel* newUser) 
+{
+	if (listUser.size() >= 4)
+		throw std::exception("La partie est complète (4 participants)");
+	listUser.push_back(newUser);
+}
 
 void GameModel::removeBot()
-{}
+{
+	/* Pas important pour l'instant : on ajoute les bots au moment du lancement de la partie -> donc le nombre de places libres et ensuite on ne les retire plus */
+}
 
 bool GameModel::createGame(string name)
 {
@@ -19,7 +43,9 @@ bool GameModel::createGame(string name)
 }
 
 void GameModel::createMap(string mod, string mapSkin)
-{}
+{
+
+}
 
 void GameModel::startMap()
 {}
--- Server/ServerModel/ServerModel/GameModel.h
+++ Server/ServerModel/ServerModel/GameModel.h
@@ -4,15 +4,22 @@
 // Game.h
 #include <iostream>
 #include <string>
+#include <vector>
+
+#include <Ice/Ice.h>
+
+class UserModel;
 
 using namespace std;
 
 class GameModel
 {
 	public:
-		GameModel();
+		//GameModel();
+		GameModel(UserModel* creator, ::Ice::ObjectAdapterPtr obj);
 		~GameModel();
 		void addBot();
+		void addUser(UserModel* newUser);
 		void removeBot();
 		bool createGame(string name);
 		void createMap(string mod, string mapSkin);
@@ -35,6 +42,8 @@ class GameModel
 		void setState(int newState);
 
 	private:
+		vector<UserModel*> listUser;
+		UserModel* gameCreator;
 		string name;
 		int nbRound;
 		int state;
