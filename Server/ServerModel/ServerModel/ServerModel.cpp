0f89d7cff005b4eae4ee9be69e73d7becc5c9a36
danyberlemont <danyberlemont@gmail.com>
2012-06-28 09:13:10 +0000

    Mise en place des classes pour le dev du serveur

--- Server/ServerModel/ServerModel/ServerModel.cpp
+++ Server/ServerModel/ServerModel/ServerModel.cpp
@@ -1,12 +1,11 @@
-// Il s'agit du fichier DLL principal.
-
-#include "stdafx.h"
-
 #include "ServerModel.h"
 
-ServerModel::ServerModel()
+ServerModel::ServerModel() : m_adapter(NULL)
 {
 }
+
+ServerModel::ServerModel(::Ice::ObjectAdapterPtr a) : m_adapter(a)
+{}
 
 ServerModel::~ServerModel()
 {
@@ -30,4 +29,58 @@ ServerModel::removeGame(string name)
 
 ServerModel::sendInvitationToPlayer(PlayerModel player, Game g)
 {
+}
+
+BomberLoutreInterface::UserData 
+ServerModel::connect(const std::string& login, const std::string& password, const Ice::Current&)
+{
+	BomberLoutreInterface::UserData us;
+	std::cout << login << " " << password << std::endl;
+	us.gameTag = "meuh";
+	return us;
+}
+
+BomberLoutreInterface::UserData
+ServerModel::createUser(const std::string& login, const std::string& password, const Ice::Current&)
+{
+	BomberLoutreInterface::UserData us;
+	return us;
+}
+
+bool 
+ServerModel::deleteUser(const std::string& login, const Ice::Current&)
+{
+	return false;
+}
+
+BomberLoutreInterface::GameInterfacePrx
+ServerModel::addGame(const std::string& name, 
+	const ::BomberLoutreInterface::GameWaitRoomPrx& room, 
+	const ::BomberLoutreInterface::MapObserverPrx& mapobs, const Ice::Current&)
+{
+	BomberLoutreInterface::GameInterfacePrx p;
+	return p;
+}
+
+BomberLoutreInterface::Map 
+ServerModel::joinGame(const std::string& name, 
+	const ::BomberLoutreInterface::GameWaitRoomPrx& room, 
+	const ::BomberLoutreInterface::MapObserverPrx& mapobs, const Ice::Current&)
+{
+	BomberLoutreInterface::Map m;
+	return m;
+}
+
+BomberLoutreInterface::GameDataList 
+ServerModel::getGameList(const Ice::Current&)
+{
+	BomberLoutreInterface::GameDataList list;
+	return list;
+}
+
+BomberLoutreInterface::UserDataList 
+ServerModel::getUserList(const Ice::Current&)
+{
+	BomberLoutreInterface::UserDataList list;
+	return list;
 }
