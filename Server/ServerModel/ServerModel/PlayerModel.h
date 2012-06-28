d1fad4554a44efeb095a3a424825ad9617a3b7c0
paul.jay.78 <paul.jay.78@gmail.com>
2012-06-28 12:49:15 +0000

    implémentation methodes

--- Server/ServerModel/ServerModel/PlayerModel.h
+++ Server/ServerModel/ServerModel/PlayerModel.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include "Bonus.h"
 #include "MapModel.h"
+#include "UserModel.h"
 #include <vector>
 
 using namespace System;
@@ -20,12 +21,15 @@ class PlayerModel
 		
 	protected :
 		MapModel *map;
+		UserModel* user;
 		int flamePower;
 		bool invincible;
 		bool invisibility;
 		bool kicker;
 		int nbBomb;
+		int nbBombUsed;
 		int nbDeath;
+		int nbSuicide;
 		int nbKill;
 		int posX;
 		int posY;
@@ -36,25 +40,33 @@ class PlayerModel
 	public :
 		enum playerDirectionId { dirLeft=0, dirRight=1, dirUp=2, dirDown=3 };
 		//PlayerModel();
-		PlayerModel(MapModel* map, int newWidth, int newHeight);
-		PlayerModel(MapModel* map, int width, int height, int flamePower,bool invincible,	bool invisibility,	bool kicker,	int nbBomb,	int nbDeath, int nbKill, int posX, int posY, int speed, bool alive);		
+		PlayerModel(MapModel* map, UserModel* user, int newWidth, int newHeight);
+		PlayerModel(MapModel* map, UserModel* user, int width, int height, int flamePower,bool invincible,	bool invisibility,	bool kicker,	int nbBomb,	int nbDeath, int nbKill, int posX, int posY, int speed, bool alive, int nbSuicide, int nbBombUsed);		
 
 		void addBonus(Bonus* b);
 		void die(BombItem* bomb);
-		void draw();
 		bool hasBonus(Bonus* b);
 		
 		void dropBomb();
-		void kickBomb();
 		void dropBonus();
 		
-		void win();
-		void loose();
 		
 		void moveUp();
 		void moveDown();
 		void moveLeft();
 		void moveRight();
+
+		/* GETTERS */
+		int getPosX();
+		int getPosY();
+		int getNbBomb();
+		void incNbBomb();
+		void decNbBomb();
+		int getNbBombUsed();
+		void incNbBombUsed();
+		void decNbBombUsed();
+		bool getKicker();
+		bool isAlive();
 };
 
 #endif
