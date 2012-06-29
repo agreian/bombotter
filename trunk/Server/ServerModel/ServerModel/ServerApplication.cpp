a54fe4e92fd6de6f0d29d36a3fc782058c199859
florent.brouca@gmail.com <florent.brouca@gmail.com@f222467b-8754-c790-d948-9a5a32e0f2da>
2012-06-29 09:08:24 +0000

    Maj

--- trunk/Server/ServerModel/ServerModel/ServerApplication.cpp
+++ trunk/Server/ServerModel/ServerModel/ServerApplication.cpp
@@ -8,17 +8,21 @@ BomberServerApplication::~BomberServerApplication()
 	
 int BomberServerApplication::run(int argc, char** argv)
 {
-	std::cout << "My server is running !!" << std::endl;
+	std::cout << "BomberLoutre server begins" << std::endl;
 	
 	m_adapter = communicator()->createObjectAdapter("BomberloutreServer");
-	
+	std::cout << "Adapter OK" << std::endl;
+
 	m_server = new ServerModel(m_adapter);
 	m_adapter->add(m_server,communicator()->stringToIdentity("BomberServer"));
-	
+	std::cout << "Object added" << std::endl;
+
 	m_adapter->activate();
+	std::cout << "Adapter activated" << std::endl;
 	
 	callbackOnInterrupt();
 	communicator()->waitForShutdown();
+	std::cout << "BomberLoutre server ends" << std::endl;
 	return EXIT_SUCCESS;
 }
 
